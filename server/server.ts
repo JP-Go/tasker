7979503800bfaffc8f3be177cc9bdcf8638114b9
João Pedro <jpedro1580@gmail.com>
2023-05-23 00:12:32 -0300

    Add cors to the server
    Add the cors package to the server to allow for cross origin requests

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import { taskRouter } from "./routers"
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/tasks',taskRouter)
 
 export { app }
