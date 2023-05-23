4f4ba9a1b04eaa89965a11598039c491d2e4d634
João Pedro <jpedro1580@gmail.com>
2023-05-23 00:11:58 -0300

    Add the react-query client
    Wrap the aplication in the QueryClientProvider and set the queryClient options.
    I Opted to not referch on mount or on Window Focus to prevent unnecessary requests

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
