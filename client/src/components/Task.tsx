0e160857f73dd6f6002a27bc914af30da026613d
João Pedro <jpedro1580@gmail.com>
2023-05-23 00:09:19 -0300

    Hook up the Task and TaskCheckbox components with data
    Adds the posibility to update the task title. When the user presses Enter, the data new title
    is sent to the server and updates the corresponding task. Also, the checkbox and trash can
    icon are functional, setting the completed status of a task and deleting it respectivelly

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,18 +1,62 @@
 import { Trash } from "phosphor-react"
-import { Task as TaskType } from "../@types/Task"
+import { TaskDTO, Task as TaskType } from "../@types/Task"
 import { TaskCheckbox } from "./TaskCheckbox"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { deleteTask, updateTask } from "../api/task-api"
 import colors from 'tailwindcss/colors'
+import { useRef, useState } from "react"
 
 interface TaskProps {
   task: TaskType,
 }
 
 export function Task({ task }: TaskProps) {
-  return <form className="w-full h-12 p-2 text-slate-700 bg-white rounded-md shadow border border-slate-300 flex justify-between items-center">
-    <input className={`flex-1 ${task.concluida ? "line-through" : ""}`} value={task.titulo} />
+  const queryClient = useQueryClient()
+
+  const [title, setTitle] = useState(task.titulo)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const updateTaskMutation = useMutation({
+    mutationFn: (taskDto: TaskDTO) => updateTask(task.id, taskDto),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["get-all-tasks"]
+      })
+    }
+  })
+
+  const deleteTaskMutation = useMutation({
+    mutationFn: deleteTask,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["get-all-tasks"]
+      })
+    }
+  })
+
+  return <form
+    className="w-full h-12 p-2 text-slate-700 bg-white rounded-md shadow border border-slate-300 flex justify-between items-center"
+    onSubmit={(e) => {
+      e.preventDefault()
+      updateTaskMutation.mutate({
+        titulo: title,
+        concluida: task.concluida
+      })
+      inputRef.current?.blur()
+    }}>
+    <input
+      ref={inputRef}
+      className={`flex-1 ${task.concluida ? "line-through" : ""}`} value={title} onChange={(e) => setTitle(e.target.value)} />
     <div className="flex gap-2 items-center h-full ">
-      <TaskCheckbox completed={task.concluida} clickHandle={() => { }} />
-      <Trash className="px-2 rounded inline-block" size={40} color={colors["red"]["500"]} weight="bold" />
+      <TaskCheckbox checked={task.concluida} clickHandle={(newState: boolean) => {
+        updateTaskMutation.mutate({
+          titulo: title,
+          concluida: newState
+        })
+      }} />
+      <Trash className="px-2 rounded inline-block cursor-pointer" size={40} color={colors["red"]["500"]} weight="bold"
+        onClick={() => deleteTaskMutation.mutate(task.id)}
+      />
     </div>
   </form>
 }
--- client/src/components/TaskCheckbox.tsx
+++ client/src/components/TaskCheckbox.tsx
@@ -1,20 +1,21 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
-import { useState } from "react";
 import color from "tailwindcss/colors"
 
 interface TaskCheckboxProps {
-  completed: boolean,
-  clickHandle: () => void
+  checked: boolean,
+  clickHandle: (newState: boolean) => void
 }
 
 export function TaskCheckbox(props: TaskCheckboxProps) {
-  const [checked, setChecked] = useState(false)
-  return <Checkbox.Root className={`border border-slate-700 w-6 h-6 rounded bg-slate-700 flex items-center justify-center ${checked ? "bg-slate-700" : "bg-white"} transition-colors`}
-    checked={checked}
-    onClick={() => setChecked((c) => !c)} >
+
+  return <Checkbox.Root className={`border border-slate-700 w-6 h-6 rounded bg-slate-700 flex items-center justify-center ${props.checked ? "bg-slate-700" : "bg-white"} transition-colors`}
+    checked={props.checked}
+    onClick={() => {
+      props.clickHandle(!props.checked)
+    }} >
     <Checkbox.Indicator>
-      <Check color={color["white"]} size={20} weight="bold"/>
+      <Check color={color["white"]} size={20} weight="bold" />
     </Checkbox.Indicator>
   </Checkbox.Root>
 }
